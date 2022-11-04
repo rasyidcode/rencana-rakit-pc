ae1f699f781f46bc4d869fcccaa2e9bd752d3834
rasyidcode <rasyidcode7@gmail.com>
2022-11-04 09:16:48 +0700

    minor changes on list rencana ui

---
+++ src/components/ItemRencana.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="px-10 py-3 active:bg-gray-200
+        transition duration-300 ease-in-out">
+        <h3 class=" font-bold uppercase">Rakitan PC - INTEL CORE I9 13722K - GTX 4090 - 64 GB RAM</h3>
+
+        <div class=" flex justify-between py-2">
+            <h4 class=" font-light">Estimasi Harga: <span class=" font-bold text-emerald-700 tracking-wider">13jt -
+                    23jt</span></h4>
+            <div>
+                <font-awesome-icon icon="fa-solid fa-pencil" class=" text-orange-500 text-xl mr-4
+                            cursor-pointer hover:text-orange-700
+                            transition duration-150 ease-in-out" />
+                <font-awesome-icon icon="fa-solid fa-trash" class=" text-red-500 text-xl
+                            cursor-pointer hover:text-red-700
+                            transition duration-150 ease-in-out" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ItemRencana'
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,26 +17,19 @@
             </div>
         </div>
         <div class="mt-5 divide-y h-4/5 flex flex-col overflow-y-scroll scrollbar">
-            <div class="px-10 py-3" v-for="i in 10" :key="i">
-                <h3 class=" font-bold uppercase">Rakitan PC - INTEL CORE I9 13722K - GTX 4090 - 64 GB RAM</h3>
-
-                <div class=" flex justify-between py-2">
-                    <h4 class=" font-light">Estimasi Harga:  <span class=" font-bold text-emerald-700 tracking-wider">13jt - 23jt</span></h4>
-                    <div>
-                        <font-awesome-icon icon="fa-solid fa-pencil" class=" text-orange-500 text-xl mr-4
-                            cursor-pointer" />
-                        <font-awesome-icon icon="fa-solid fa-trash" class=" text-red-500 text-xl
-                            cursor-pointer" />
-                    </div>
-                </div>
-            </div>
+            <ItemRencana v-for="i in 10" :key="i" />
         </div>
     </div>
 </template>
 
 <script>
+import ItemRencana from '../components/ItemRencana.vue';
+
 export default {
     name: 'HomeView',
+    components: {
+        ItemRencana
+    },
     data() {
         return {
             isActive: true
