11a9d71996afaa7fd7c737eadbe366541c90799e
rasyidcode <rasyidcode7@gmail.com>
2022-11-26 22:19:02 +0700

    add page loading

--- src/views/Komponen/ListKomponenView2.vue
+++ src/views/Komponen/ListKomponenView2.vue
@@ -24,8 +24,15 @@
                 :price-id="komponen.prices[0].id" @item-hapus="showAlertHapus(komponen.id, komponen.ref)" />
         </div>
         <div v-else class="flex justify-center items-center h-full">
-            <h3 class="font-normal text-base text-gray-600
+            <h3 v-if="!isPageLoading" class="font-normal text-base text-gray-600
                 tracking-wider">Belum ada data</h3>
+            <svg v-else class="animate-spin h-10 w-10 text-emerald-500" xmlns="http://www.w3.org/2000/svg"
+                fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                </path>
+            </svg>
         </div>
 
         <ScrollLoading :isLoading="isLoading" />
@@ -61,10 +68,13 @@ export default {
             isLoading: false,
             isLastData: false,
             showAlert: false,
-            hapusData: null
+            hapusData: null,
+            isPageLoading: false
         }
     },
     async mounted() {
+        this.isPageLoading = true;
+
         try {
             const docsSnapshot = await getDocs(query(componentsCollection, where('type', '==', this.$route.params.type), orderBy('createdAt', 'asc'), limit(10)));
             this.components = docsSnapshot.docs.map(doc => {
@@ -76,8 +86,11 @@ export default {
                 component.prices.sort((a, b) => a.checkedAt < b.checkedAt ? 1 : -1);
                 return component;
             });
+
+            this.isPageLoading = false;
         } catch (e) {
             console.log(e);
+            this.isPageLoading = false;
         }
     },
     methods: {
