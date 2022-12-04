6059860e085aed64022d05b7ee543fd99bcefee3
rasyidcode <rasyidcode7@gmail.com>
2022-12-04 16:34:33 +0700

    test neovim

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-red-500 rounded-xl">
+    Hello, World! 
+    <span v-for="(item, i) in testData" :key="i">{{ item }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent', 
+  data() {
+    return {
+      testData: [
+        "test1",
+        "test2"
+      ]
+    }
+  },
+}
+</script>
